cf0d4cc37cd698d96d1d3af11ac9db724751b43b
Connor Vitz <connorvitz@gmail.com>
2025-04-01 13:12:49 -0400

    Condensed BasicCareer state to just two questions. Made sliders functional.

--- src/BasicCareer.tsx
+++ src/BasicCareer.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 interface Slider {
     option: string,
@@ -23,58 +23,69 @@ export function BasicCareer(): React.JSX.Element {
     const [question1, setQuestion1] = useState<BasicQuestionType>(init_question);
     const [question2, setQuestion2] = useState<BasicQuestionType>(init_question);
 
-    function newQuestion(): BasicQuestionType {
+    function updateQuestion1(option: string, event: React.ChangeEvent<HTMLInputElement>) {
+        const new_sliders1: Slider[] = question1.sliders.map((slider: Slider): Slider => (slider.option === option) ? {option: slider.option, value: event.target.value} : slider);
+        setQuestion1({...question1, sliders: new_sliders1}); 
+    }
+    function updateQuestion2(option: string, event: React.ChangeEvent<HTMLInputElement>) {
+        const new_sliders2: Slider[] = question2.sliders.map((slider: Slider): Slider => (slider.option === option) ? {option: slider.option, value: event.target.value} : slider);
+        setQuestion2({...question1, sliders: new_sliders2}); 
+    }
+
+    function newQuestions() {
         const new_sliders: Slider[] = [
             { option: 'e', value: "0" },
             { option: 'f', value: "0" },
             { option: 'g', value: "0" },
             { option: 'h', value: "0" }
-        ]
-        return {description: "E, F, G, or H?", sliders: new_sliders}
-    }
-    function updateQuestion1(new_question: BasicQuestionType){
+        ];
+        const new_question: BasicQuestionType = {description: "E, F, G, or H?", sliders: new_sliders};
         setQuestion1(new_question);
-    }
-    function updateQuestion2(new_question: BasicQuestionType){
         setQuestion2(new_question);
     }
 
     return (
         <div>
             <h1>Basic Career Assessment</h1>
-            <BasicQuestion question={question1}></BasicQuestion>
-            <Button onClick={()=>{updateQuestion1(newQuestion())}}>Submit Answer</Button>
-            <BasicQuestion question={question2}></BasicQuestion>
-            <Button onClick={()=>{updateQuestion2(newQuestion())}}>Submit Answer</Button>
-        </div>
-    );
-};
-
-function BasicQuestion({question}: {question: BasicQuestionType}): React.JSX.Element {
-    const [sliders, setSliders] = useState<Slider[]>(question.sliders);
-
-    function updateSlider(option: string, event: React.ChangeEvent<HTMLInputElement>) {
-        const new_sliders: Slider[] = sliders;
-        /* sliders.map((slider: Slider): Slider => (slider.option === option) ? {option: slider.option, value: event.target.value} : slider); */
-        setSliders(new_sliders);
-    }
-
-    return (
-        <div>
-            <h3>{question.description}</h3>
-            <div>
-                {sliders.map((slider: Slider) => (
-                    <div><Form.Range
-                        id={slider.option}
-                        min="0"
-                        max="5"
-                        step="1"
-                        onChange={(event) => updateSlider(slider.option, event)}
-                        value={slider.value}
-                        style={{width: "200px"}}
-                    /></div>
-                ))}
+            <div style={{display: "flex", padding: "20px", justifyContent: "center"}}>
+                <div id="question" style={{padding: "10px"}}>
+                    <h3>{question1.description}</h3>
+                    <div>
+                        {question1.sliders.map((slider: Slider) => (
+                            <div> {slider.option}
+                                <Form.Range
+                                id={slider.option}
+                                min="0"
+                                max="5"
+                                step="1"
+                                onChange={(event) => updateQuestion1(slider.option, event)}
+                                value={slider.value}
+                                style={{width: "200px"}}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div id="question" style={{padding: "10px"}}>
+                    <h3>{question2.description}</h3>
+                    <div>
+                        {question2.sliders.map((slider: Slider) => (
+                            <div> {slider.option}
+                                <Form.Range
+                                id={slider.option}
+                                min="0"
+                                max="5"
+                                step="1"
+                                onChange={(event) => updateQuestion2(slider.option, event)}
+                                value={slider.value}
+                                style={{width: "200px"}}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
+            <Button onClick={()=>{newQuestions()}}>Submit Answer</Button>
         </div>
     );
 };
