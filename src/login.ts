2c3e65b8ad2a95ea4528936246c20440fb1a9bae
Connor Vitz <connorvitz@gmail.com>
2025-05-05 13:14:59 -0400

    Created two functions for use in creating a user. One to read login.json and create a list of current users, and another to return a User object with given username.

--- src/login.ts
+++ src/login.ts
@@ -1,8 +1,15 @@
 import fs from 'fs';
 
-type User = {username: string, password: string}
+type User = {username: string, password: string} | undefined;
 
 export function create_login(username: string, password: string) {
+    let login_data: User[] = read_login_data();
+
+    if (find_username(login_data,username)) {
+        alert("Username is taken");
+        return;
+    }
+
     let user_login: User = {
         username: username,
         password: password
@@ -13,7 +20,7 @@ export function create_login(username: string, password: string) {
         userJson = JSON.stringify(user_login);
     } catch (error) {
         console.log(error);
-        alert("User signup was invalid")
+        alert("User signup was invalid");
     }
     
 
@@ -24,4 +31,22 @@ export function create_login(username: string, password: string) {
             console.log('Successfully wrote file');
         }
     });
+}
+
+function read_login_data(): User[] {
+    let data: string = "";
+    try {
+        data = fs.readFileSync("./login.json", { encoding: 'utf8', flag: 'r' });
+    } catch (error) {
+        console.error('Error reading input2.txt:', error);
+    }
+    let data_array: string[] = data.split("}");
+    let login_data: User[] = data_array.map(string => JSON.parse(string + "}"));
+    return login_data;
+}
+
+function find_username(login_data: User[], username: string): User {
+    let user: User = undefined;
+    user = login_data.find(login => (login) ? login.username === username : false);
+    return user
 }
